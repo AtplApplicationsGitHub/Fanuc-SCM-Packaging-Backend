9b5fc64408f0deb8a38770ab687b2fdec9462c86
pranavalternative <pranav.r@adventsys.co>
2025-12-24 11:49:42 +0530

    Remove client folder from backend repo

--- client/src/constants/roles.js
+++
@@ -1,7 +0,0 @@
-/* client/src/constants/roles.js */
-export const ROLES = {
-  SCM_ADMIN: 'SCM Admin',
-  SALES_ENGINEER: 'Sales Engineer',
-  SALES_MANAGER: 'Sales Manager',
-  MANAGEMENT: 'Management',
-};
